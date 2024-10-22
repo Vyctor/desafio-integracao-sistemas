a2985c8e277953a31621375eae7a9abb3278decc
Vyctor <dev.vyctor@gmail.com>
2024-10-22 02:07:06 -0300

    feat: extraindo metodo de transformacao de arquivo para service

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -16,29 +16,9 @@ export class OrdersController {
   @Post('process')
   @UseInterceptors(FileInterceptor('file'))
   async process(@UploadedFile() file: Express.Multer.File) {
-    const fileData = file.buffer.toString('utf-8').split('\n');
-    const orders = fileData
-      .map((line, index) => {
-        if (index === fileData.length - 1) {
-          return;
-        }
-        const userId = parseInt(line.substring(0, 10).trim(), 10);
-        const name = line.substring(10, 55).trim();
-        const orderId = parseInt(line.substring(55, 65).trim(), 10);
-        const prodId = parseInt(line.substring(65, 75).trim(), 10);
-        const value = parseFloat(line.substring(75, 87).trim());
-        const date = new Date(
-          line
-            .substring(87, 95)
-            .trim()
-            .replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-        );
-
-        return { userId, name, orderId, prodId, value, date };
-      })
-      .filter((order) => order);
-
-    return this.ordersService.saveOrders(orders);
+    return this.ordersService.saveOrders(
+      this.ordersService.transformOrdersFileToJson(file),
+    );
   }
 
   @Get()
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -92,4 +92,37 @@ export class OrdersService {
 
     return result;
   }
+
+  transformOrdersFileToJson(file: Express.Multer.File): Array<{
+    userId: number;
+    name: string;
+    orderId: number;
+    prodId: number;
+    value: number;
+    date: Date;
+  }> {
+    const fileData = file.buffer.toString('utf-8').split('\n');
+    const orders = fileData
+      .map((line, index) => {
+        if (index === fileData.length - 1) {
+          return;
+        }
+        const userId = parseInt(line.substring(0, 10).trim(), 10);
+        const name = line.substring(10, 55).trim();
+        const orderId = parseInt(line.substring(55, 65).trim(), 10);
+        const prodId = parseInt(line.substring(65, 75).trim(), 10);
+        const value = parseFloat(line.substring(75, 87).trim());
+        const date = new Date(
+          line
+            .substring(87, 95)
+            .trim()
+            .replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+        );
+
+        return { userId, name, orderId, prodId, value, date };
+      })
+      .filter((order) => order);
+
+    return orders;
+  }
 }
